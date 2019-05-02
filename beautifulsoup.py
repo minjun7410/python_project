d0fb2eb84f706da561d57bc3b933e6a85a2df1a5
kieron <minjun7410@kookmin.ac.kr>
2019-05-02 09:28:28 +0900

    new git environ

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-html = urlopen("https://cs.kookmin.ac.kr/")
+html = urlopen("https://cs.kookmin.ac.kr/")#python insterpreter
 bsObject = BeautifulSoup(html, "html.parser")
 
 print(bsObject)
